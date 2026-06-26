b46eeee9de310e740f02f5ad4d9934e6dc80aa83
MauGu25 <2509090@upy.edu.mx>
2026-06-25 21:37:35 -0600

    Add Classwork 09 - Spanish Verb Conjugator

---
+++ Classwork-09-Spanish-Verb-Conjugator/spanish_verb_conjugator.py
@@ -0,0 +1,20 @@
+# Required Structures
+pronouns = ["Yo", "Tú", "Él", "Nosotros", "Vosotros", "Ellos"]
+
+endings = {
+    'ar': ['o', 'as', 'a', 'amos', 'ais', 'an'],
+    'er': ['o', 'es', 'e', 'emos', 'eis', 'en'],
+    'ir': ['o', 'es', 'e', 'imos', 'is', 'en']
+}
+
+# INPUT
+verb = input("Write a spanish verb (ar/er/ir): ")
+
+stem = verb[:-2] # Get the stem
+ending = verb[-2:] # Get the ending
+
+conjugations = endings[ending]
+
+for index, pronoun in enumerate(pronouns):
+    termination = conjugations[index]
+    print(f"{pronoun} {stem}{termination}")
